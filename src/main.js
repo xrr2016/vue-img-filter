98b9790e30de44c8a453220c9127d208da2e3dcc
xrr2016 <xiaoranran1993@163.com>
2018-09-03 02:15:29 +0800

    :tada: init commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
 import App from './App.vue'
 import './registerServiceWorker'
+import './main.css'
 
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
