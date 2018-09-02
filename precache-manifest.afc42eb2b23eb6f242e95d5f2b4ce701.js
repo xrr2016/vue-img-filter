acea3edf182c64413911851bbfe4e24851a3aa28
xrr2016 <xiaoranran1993@163.com>
2018-09-03 03:53:18 +0800

    deploy

---
+++ precache-manifest.afc42eb2b23eb6f242e95d5f2b4ce701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://xrr2016.github.io/vue-img-filter/robots.txt"
+  },
+  {
+    "revision": "efac033f3739aade426a",
+    "url": "https://xrr2016.github.io/vue-img-filter/js/chunk-vendors.810391d5.js"
+  },
+  {
+    "revision": "a3e94f8e5d4d22414358",
+    "url": "https://xrr2016.github.io/vue-img-filter/js/app.7ee54864.js"
+  },
+  {
+    "revision": "b32ab8e0c178a81d0589032a6f6bc7b9",
+    "url": "https://xrr2016.github.io/vue-img-filter/index.html"
+  },
+  {
+    "revision": "efac033f3739aade426a",
+    "url": "https://xrr2016.github.io/vue-img-filter/css/chunk-vendors.5769f3c8.css"
+  },
+  {
+    "revision": "a3e94f8e5d4d22414358",
+    "url": "https://xrr2016.github.io/vue-img-filter/css/app.6d639d02.css"
+  }
+];
